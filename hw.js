0c724dcc0a3790eb7ead98d55648c89be457fd12
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-10-20 15:25:36 -0500

    added the missing homework

--- hw.js
+++ hw.js
@@ -57,7 +57,7 @@ console.log(`${quartersInChange} `);
 //4.Write a function that prints out how many bills you would recieve in change after paying a certain amount.  
 //Return in as large denominations as possible. Parameters should be amount paind and amount cost
 
-
+/*
 function calculateChange(billAmount, itemPrice) {   //function that calculate the change
     const billchange = billAmount - itemPrice;
     
@@ -86,3 +86,25 @@ console.log("Change in Bills:");
 for (const denomination in billchange) {
     console.log(`$${denomination} bills: ${billchange[denomination]}`);
 }
+*/
+
+
+
+// Write a program that uses console.log to print all the numbers 
+//from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, 
+//and for numbers divisible by 5 (and not 3),print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", 
+//for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+/*
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  */
+  
